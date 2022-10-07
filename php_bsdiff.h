6bdf11b602828f2bae38438f2a230a6f327c714f
Demin Yin <deminy@deminy.net>
2022-10-07 15:36:24 -0700

    add PECL package information and prepare for release v0.1.1

--- php_bsdiff.h
+++ php_bsdiff.h
@@ -22,7 +22,7 @@
 extern zend_module_entry bsdiff_module_entry;
 # define phpext_bsdiff_ptr &bsdiff_module_entry
 
-#define PHP_BSDIFF_VERSION "0.1.0"
+#define PHP_BSDIFF_VERSION "0.1.1"
 
 # if defined(ZTS) && defined(COMPILE_DL_BSDIFF)
 ZEND_TSRMLS_CACHE_EXTERN()
