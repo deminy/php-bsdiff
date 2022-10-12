a572f732fe373e9df13f8ae1c7010a5b1b51e710
Demin Yin <deminy@deminy.net>
2022-10-12 14:57:01 -0700

    prepare for release v0.1.2

--- php_bsdiff.h
+++ php_bsdiff.h
@@ -22,7 +22,7 @@
 extern zend_module_entry bsdiff_module_entry;
 # define phpext_bsdiff_ptr &bsdiff_module_entry
 
-#define PHP_BSDIFF_VERSION "0.1.2-dev"
+#define PHP_BSDIFF_VERSION "0.1.2"
 
 # if defined(ZTS) && defined(COMPILE_DL_BSDIFF)
 ZEND_TSRMLS_CACHE_EXTERN()
